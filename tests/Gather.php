4265e0dc362fcd93d9af983c90facfff81b2646a
Emir Kurtovic <rapttor@gmail.com>
2023-09-29 01:30:29 +0200

    E:

--- tests/Gather.php
+++ tests/Gather.php
@@ -5,6 +5,10 @@ require_once(__DIR__ . "/../src/Proxy.php");
 defined("DEBUG") || define("DEBUG", 1);
 
 $p = new RapTToR\Proxy;
+$all = $p->loadAll();
+var_dump($all);
+
+## test
 
 $p = (new \RapTToR\Proxy)->get(
     array(
